e3296c5e570bb3d3e8c0a1ac77385cf80e27f5f1
Souhail <mr.souid@live.fr>
2018-09-19 05:17:38 +0200

    second commit

---
+++ src/__tests__/integrations.test.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import moxios from 'moxios'
+import { mount } from 'enzyme'
+import Root from 'Root'
+import App from 'components/App'
+import { doesNotReject } from 'assert'
+beforeEach(() => {
+	moxios.install()
+	moxios.stubRequest('https://jsonplaceholder.typicode.com/comments', {
+		status: 200,
+		response: [ { name: 'Fetched #1' }, { name: 'Fetched #2' } ]
+	})
+})
+afterEach(() => {
+	moxios.uninstall()
+})
+it('can fetch a list of comments and display them', () => {
+	const wrapped = mount(
+		<Root>
+			<App />
+		</Root>
+	)
+	wrapped.find('.fetch-comments').simulate('click')
+	moxios.wait(() => {
+		wrapped.update()
+		expect(wrapped.find('li').length).toEqual(2)
+		done()
+		wrapped.unmount()
+	})
+})
---
+++ src/reducers/__tests__/comments.test.js
@@ -0,0 +1,17 @@
+import CommentsReducer from 'reducers/comments'
+import { SAVE_COMMENT } from 'actions/types'
+
+it('handles actions of type SAVE_COMMENT', () => {
+	const action = {
+		type: SAVE_COMMENT,
+		payload: 'New Comment'
+	}
+
+	const newState = CommentsReducer([], action)
+
+	expect(newState).toEqual([ 'New Comment' ])
+})
+it('handles actions with unknown type', () => {
+	const newState = CommentsReducer([], { type: '' })
+	expect(newState).toEqual([])
+})
