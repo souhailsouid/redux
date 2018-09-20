e0bf821fa582cf799552bed70963b9800da54a1c
Souhail <mr.souid@live.fr>
2018-09-20 14:15:40 +0200

    apply middlewares

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import reduxPromise from 'redux-promise'
+import async from 'middlewares/async'
 import reducers from 'reducers'
-
+import stateValidator from 'middlewares/stateValidator'
 export default ({ children, initialState = {} }) => {
-	const store = createStore(reducers, initialState, applyMiddleware(reduxPromise))
+	const store = createStore(reducers, initialState, applyMiddleware(async), stateValidator)
 	return <Provider store={store}>{children}</Provider>
 }
